ab864eb4a4a7996f68f134c9ca4714aa7e2a8463
Klym Andrii <klym.andriy14@gmail.com>
2022-08-29 19:02:19 +0300

    change readme

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -10,7 +10,7 @@ const Map = () => {
     const classes = useStyles();
     const isMobele = useMediaQuery('(min-width:600px)');
 
-    const coordinates = { lat: 0, lng: 0 };
+    const coordinates = { lat: 49.8397, lng: 24.0297 };
 
     return (
         <div className={classes.mapContainer}>
