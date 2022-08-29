506bd5ad36f1422f2ee8c62756a45692cccf1836
Klym Andrii <klym.andriy14@gmail.com>
2022-08-29 18:48:36 +0300

    create react app and added google maps api

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Topography,  useMediaQuery } from "@material-ui/core"
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab"
+
+import useStyles from "./styles"
+
+const Map = () => {
+    const classes = useStyles();
+    const isMobele = useMediaQuery('(min-width:600px)');
+
+    const coordinates = { lat: 0, lng: 0 };
+
+    return (
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyA5Ha1nXmA09Zn7cF8rUKurK84nU5Au2sk' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
+    )
+}
+
+export default Map;
